89d2860c55b1d0d1813c098aa981d5818ff6da5a
exiler <shaukoay.dev@gmail.com>
2026-05-07 17:33:20 +0800

    Initialize database connection and set up server

--- src/init-db.ts
+++ src/init-db.ts
@@ -1,1 +1,17 @@
 /* Create database table automatically */
+import sql from "./db.js";
+
+export const initDb = async () => {
+    await sql`
+        CREATE TABLE IF NOT EXISTS flags (
+            id SERIAL PRIMARY KEY,
+            key TEXT UNIQUE NOT NULL,
+            description TEXT,
+            enabled BOOLEAN DEFAULT false,
+            environment TEXT DEFAULT 'dev',
+            created_at TIMESTAMP DEFAULT NOW()
+        )
+    `;
+
+    console.log("Database intialised");
+};
--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,32 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import dotenv from "dotenv";
 
+import { initDb } from "./init-db.js";
+
 dotenv.config()
 
-const app = Fastify({
-    logger: true
-});
+const start = async ()=> {
+    const app = Fastify({
+        logger: true
+    });
+    
+    await app.register(cors);
 
-await app.register(cors);
+    await initDb();
+    
+    app.get("/health", async () => {
+        return {
+            status: "ok",
+            service: "liveflag",
+        };
+    });
+    
+    const port = Number(process.env.PORT ?? 3000);
+    
+    await app.listen({
+        port,
+        host: "0.0.0.0"
+    });
+};
 
-app.get("/health", async () => {
-    return {
-        status: "ok",
-        service: "liveflag",
-    };
-});
-
-const port = Number(process.env.PORT ?? 3000);
-
-await app.listen({
-    port,
-    host: "0.0.0.0"
-})
+start();
